c4fac51311dbbbdd757dcd1513029acc0ac5ddd8
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-05-29 23:46:52 -0400

    get all four buttons to display answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ const pageTitle = document.getElementById('page-title');
 const instructions = document.getElementById('instructions');
 const questionContainer = document.getElementById('question-container');
 const questionEl = document.getElementById('question');
-const choiceButtonEl = document.getElementById('choice-button');
+const choiceButton1 = document.getElementById('choice-button-1');
+const choiceButton2 = document.getElementById('choice-button-2');
+const choiceButton3 = document.getElementById('choice-button-3');
+const choiceButton4 = document.getElementById('choice-button-4');
 const answerContainer = document.getElementById('answer-container')
-let questionOrder, questionIndex
-
+let questionOrder, questionIndex, answerOrder, answerIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -18,17 +20,28 @@ function startGame() {
     answerContainer.classList.remove('hide')
     questionOrder = (i=0, i<questions.length, i++)
     questionIndex = 0
+    answerOrder = (i=0, i<questions[questionOrder].answerChoices.length, i++)
+    answerIndex = 0
 
-    newQuestion();   
+    newQuestion();
 };
 
 function newQuestion() {
+    
     currentQuestion(questionOrder[questionIndex])
 };
 
-function currentQuestion() {
+function currentQuestion(question) {
     questionEl.innerText = questions[questionOrder].question
-    console.log(currentQuestion.innerText)
+
+    currentAnswers();
+};
+
+function currentAnswers() {
+    choiceButton1.innerText = questions[questionOrder].answerChoices[0].choice
+    choiceButton2.innerText = questions[questionOrder].answerChoices[1].choice
+    choiceButton3.innerText = questions[questionOrder].answerChoices[2].choice
+    choiceButton4.innerText = questions[questionOrder].answerChoices[3].choice
 };
 
 const questions = [
